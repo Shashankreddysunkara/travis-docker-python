545a63dcb90791520cbb955d16f5489facc02b04
Shashank Reddy Sunkara <inusasunny@gmail.com>
2020-10-30 14:40:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -63,10 +63,10 @@ setup(
         'Programming Language :: Python :: 3.6',
 
         # Database
-        'Database :: PostgreSQL :: 9.2'
+        'Database :: PostgreSQL :: 9.2',
         
         # Configuration management tools
-        'CM tool :: Ansible :: 1.2'
+        'CM tool :: Ansible :: 1.2',
         
         # #CI-CD
         # 'CI-CD :: Jenkins :: 2.249.2'
